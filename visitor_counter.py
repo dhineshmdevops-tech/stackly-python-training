e76d598ed7b818be95f453d8f5da3eb2f9f2f92a
dhineshmdevops-tech <dhinesh.mdevops@gmail.com>
2026-04-01 21:30:21 +0530

    Delete visitor_counter.py

--- visitor_counter.py
+++
@@ -1,9 +0,0 @@
-visitors = set()
-
-while True:
-    name = input("enter visitor name (or 'exit): ")
-    if name == "exit":
-        break
-    visitors.add(name)
-
-print("total unique visitors:", len(visitors))
