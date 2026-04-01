53696eb74a20b296c37f67aab81b49ed47257721
dhineshmdevops-tech <dhinesh.mdevops@gmail.com>
2026-04-01 21:31:21 +0530

    Delete number_utility.py

--- number_utility.py
+++
@@ -1,8 +0,0 @@
-num = int(input("enter number: "))
-
-print("binary:", bin(num))
-print("octal:", oct(num))
-print("hex:", hex(num))
-
-print("with commas:", f"{num:,}")
-print("scientific:", f"{num:.2e}")
