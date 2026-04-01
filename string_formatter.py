3bb4129e6cc53bbf9323efcb0b59f454b58b7fcb
dhineshmdevops-tech <dhinesh.mdevops@gmail.com>
2026-04-01 21:30:43 +0530

    Delete string_formatter.py

--- string_formatter.py
+++
@@ -1,8 +0,0 @@
-name = input("enter name: ")
-product = input("enter product: ")
-
-print(f"{name} bought {product}")
-
-print("left:", name.ljust(20))
-print("right:", name.rjust(20))
-print("center:", name.center(20))
